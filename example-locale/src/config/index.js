4b26df0a3120b195afef638634e18a4f7b8f9ec6
Alex Patterson <alex@codingcat.dev>
2022-11-17 18:13:56 -0500

    change to use env variables

--- example-locale/src/config/index.js
+++ example-locale/src/config/index.js
@@ -2,8 +2,8 @@ const {Client, Locale} = require("appwrite"); //Import appwrite
 
 let client  = new Client();
 client
-  .setEndpoint("http://localhost/v1") // Your API Endpoint
-  .setProject("demos-for-react") // Your project ID
+  .setEndpoint(process.env.REACT_APP_APPWRITE_ENDPOINT) // Your API Endpoint
+  .setProject(process.env.REACT_APP_APPWRITE_PROJECT_ID) // Your project ID
 
 const locale = new Locale(client);
 let response = locale.get();
